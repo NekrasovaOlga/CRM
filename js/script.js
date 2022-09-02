9c36380852ca9b716c050eed579e3d4f22229716
Olga <nekrasova.kalina28@gmail.com>
2022-09-02 20:45:14 +0300

    lesson_2

---
+++ js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalDiscountCheck = document.querySelector('.modal__label_discount #discount');
+const modalDiscountInput  = document.querySelector('.modal__input_discount');
