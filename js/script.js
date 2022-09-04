101d54b56f5b4405c53be18aebbe89dc155c4ca2
Olga <nekrasova.kalina28@gmail.com>
2022-09-04 23:38:59 +0300

    модуль 5

--- js/script.js
+++ js/script.js
@@ -1,72 +1,72 @@
 'use strict';
 
+const cart = [
+  {
+    "id": 1,
+    "title": "Смартфон Xiaomi 11T 8/128GB",
+    "price": 27000,
+    "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
+    "category": "mobile-phone",
+    "discont": false,
+    "count": 3,
+    "units": "шт",
+    "images": {
+      "small": "img/smrtxiaomi11t-m.jpg",
+      "big": "img/smrtxiaomi11t-b.jpg"
+    }
+  },
+  {
+    "id": 2,
+    "title": "Радиоуправляемый автомобиль Cheetan",
+    "price": 4000,
+    "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
+    "category": "toys",
+    "discont": 5,
+    "count": 1,
+    "units": "шт",
+    "images": {
+      "small": "img/cheetancar-m.jpg",
+      "big": "img/cheetancar-b.jpg"
+    }
+  },
+  {
+    "id": 3,
+    "title": "ТВ приставка MECOOL KI",
+    "price": 12400,
+    "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
+    "category": "tv-box",
+    "discont": 15,
+    "count": 4,
+    "units": "шт",
+    "images": {
+      "small": "img/tvboxmecool-m.jpg",
+      "big": "img/tvboxmecool-b.jpg"
+    }
+  },
+  {
+    "id": 4,
+    "title": "Витая пара PROConnect 01-0043-3-25",
+    "price": 22,
+    "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
+    "category": "cables",
+    "discont": false,
+    "count": 420,
+    "units": "шт",
+    "images": {
+      "small": "img/lan_proconnect43-3-25.jpg",
+      "big": "img/lan_proconnect43-3-25-b.jpg"
+    }
+  }
+]
+
+{
+
 const modalTitle = document.querySelector('.modal__title');
 const modalForm = document.querySelector('.modal__form');
 const modalDiscountCheck = document.querySelector('.modal__label_discount #discount');
 const modalDiscountInput  = document.querySelector('.modal__input_discount');
 
-const overlay = document.querySelector('.overlay');
 
-const cart = [
-    {
-      "id": 1,
-      "title": "Смартфон Xiaomi 11T 8/128GB",
-      "price": 27000,
-      "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
-      "category": "mobile-phone",
-      "discont": false,
-      "count": 3,
-      "units": "шт",
-      "images": {
-        "small": "img/smrtxiaomi11t-m.jpg",
-        "big": "img/smrtxiaomi11t-b.jpg"
-      }
-    },
-    {
-      "id": 2,
-      "title": "Радиоуправляемый автомобиль Cheetan",
-      "price": 4000,
-      "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
-      "category": "toys",
-      "discont": 5,
-      "count": 1,
-      "units": "шт",
-      "images": {
-        "small": "img/cheetancar-m.jpg",
-        "big": "img/cheetancar-b.jpg"
-      }
-    },
-    {
-      "id": 3,
-      "title": "ТВ приставка MECOOL KI",
-      "price": 12400,
-      "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
-      "category": "tv-box",
-      "discont": 15,
-      "count": 4,
-      "units": "шт",
-      "images": {
-        "small": "img/tvboxmecool-m.jpg",
-        "big": "img/tvboxmecool-b.jpg"
-      }
-    },
-    {
-      "id": 4,
-      "title": "Витая пара PROConnect 01-0043-3-25",
-      "price": 22,
-      "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
-      "category": "cables",
-      "discont": false,
-      "count": 420,
-      "units": "шт",
-      "images": {
-        "small": "img/lan_proconnect43-3-25.jpg",
-        "big": "img/lan_proconnect43-3-25-b.jpg"
-      }
-    }
-  ]
-
-overlay.classList.remove('active');
 
 const createRow = ({id, count, description, discont, price, title, units}) => {
     const goods__table = document.querySelector('.goods__table .table__body');
@@ -94,5 +94,30 @@ const createRow = ({id, count, description, discont, price, title, units}) => {
 const renderGoods = cart => {
     const sortRow = cart.map(createRow)
 }
+const formInit = (btnAdd, overlay) => {
+  btnAdd.addEventListener('click', (event) => {
+    overlay.classList.add('active'); 
+  })
 
-renderGoods(cart)
+  overlay.addEventListener('click', (event) => {
+    const target = event.target;
+    if(!target.closest('.modal') || target.closest('.modal__close')){
+      overlay.classList.remove('active'); 
+    }
+  })
+}
+ 
+  const init = () => {
+    const btnAdd = document.querySelector('.panel__add-goods');
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('active'); 
+
+    const goods = renderGoods(cart);
+
+    const form = formInit(btnAdd, overlay);
+    
+    
+
+  }
+  init()
+}
