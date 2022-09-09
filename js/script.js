cc0e97e5dcc6ffbf406ed32ba541c5453f0edd0a
Olga <nekrasova.kalina28@gmail.com>
2022-09-09 18:15:20 +0300

    Вывод количества товара

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const cart = [
     id: 1,
     title: 'Смартфон Xiaomi 11T 8/128GB',
     price: 27000,
+    subTotal: 81000,
     description:
       'Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.',
     category: 'mobile-phone',
@@ -21,6 +22,7 @@ const cart = [
     id: 2,
     title: 'Радиоуправляемый автомобиль Cheetan',
     price: 4000,
+    subTotal: 4000,
     description:
       'Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет',
     category: 'toys',
@@ -36,6 +38,7 @@ const cart = [
     id: 3,
     title: 'ТВ приставка MECOOL KI',
     price: 12400,
+    subTotal: 49600,
     description:
       'Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D',
     category: 'tv-box',
@@ -51,6 +54,7 @@ const cart = [
     id: 4,
     title: 'Витая пара PROConnect 01-0043-3-25',
     price: 22,
+    subTotal: 9240,
     description:
       'Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.',
     category: 'cables',
@@ -68,6 +72,7 @@ const cart = [
   const createRow = ({
     id,
     count,
+    subTotal,
     description,
     discont,
     price,
@@ -84,8 +89,8 @@ const cart = [
     <td class='table__cell table__cell_left'>Техника для дома</td>
     <td class='table__cell'>${units}</td>
     <td class='table__cell'>${count}</td>
-    <td class='table__cell'></td>
     <td class='table__cell'>$${price}</td>
+    <td class='table__cell'>$${subTotal}</td>
     <td class='table__cell table__cell_btn-wrapper'>
       <button class='table__btn table__btn_pic'></button>
       <button class='table__btn table__btn_edit'></button>
@@ -129,7 +134,7 @@ const cart = [
   const totalSum = (cartTotal) => {
     const oldSum = document.querySelector('.cms__total-price');
     const newSum = cartTotal.reduce(
-        (initialValue, currentItem) => +currentItem.price + initialValue, 0);
+        (initialValue, currentItem) => +currentItem.subTotal + initialValue, 0);
 
     oldSum.textContent = `$ ${newSum}`;
   };
@@ -152,13 +157,11 @@ const cart = [
       let totalPrice;
       if (form.discount.checked) {
         form.discount_count.disabled = false;
-        totalPrice =
-          form.price.value -
-          form.price.value * (form.discount_count.value / 100);
+        totalPrice = (+form.price.value - +form.price.value * (form.discount_count.value / 100)) * +form.count.value;
       } else {
         form.discount_count.value = '';
         form.discount_count.disabled = true;
-        totalPrice = form.price.value;
+        totalPrice = +form.price.value * +form.count.value;
       }
       form.total.value = '$ ' + totalPrice;
     });
@@ -169,7 +172,8 @@ const cart = [
       const codeId = document.querySelector('.vendor-code__id');
       const formData = new FormData(e.target);
       const newItem = Object.fromEntries(formData);
-
+      const subtotal = newItem.price * newItem.count;
+      newItem.subTotal = !newItem.discount ? subtotal : subtotal - subtotal * (newItem.discount_count / 100);
       newItem.id = codeId.textContent;
       createRow(newItem);
       cart.push(newItem);
